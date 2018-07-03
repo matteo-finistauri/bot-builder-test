9ad0313db86b3a0c7864c465795a71e16e489af1
Matteo Finistauri <matteo_finistauri@hotmail.com>
2018-07-03 16:07:19 +0200

    Structured a more complex bot.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var builder = require('botbuilder');
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat connector for communicating with the Bot Framework Service
@@ -18,6 +18,47 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
-});
+var bot = new builder.UniversalBot(connector, [
+    function (session, results, next) {
+        // session.beginDialog('greetings');
+        next();
+    },
+    function (session, results) {
+        session.beginDialog('requestGuestAccess');
+    },
+    function (session, results) {
+        session.send("Results: " + JSON.stringify(results.response));
+        session.endDialog(`Thank you for coming and see you!`);
+    }
+]);
+
+// Ask the user for their name and greet them by name.
+bot.dialog('greetings', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.endDialog(`Hello ${results.response}!`);
+    }
+]);
+
+bot.dialog('requestGuestAccess', [
+    function (session) {
+        builder.Prompts.text(session, 'What is the name of the contractor?');
+    },
+    function (session, results) {
+        session.dialogData.contractorName = results.response;
+        builder.Prompts.time(session, 'What is the end of contract?');
+    },
+    function (session, results) {
+        session.dialogData.contractEnd = builder.EntityRecognizer.resolveTime([results.response]);
+        var result = { response: { name: session.dialogData.contractorName, end: session.dialogData.contractEnd } };
+        session.endDialogWithResult(result);
+    }
+]).endConversationAction(
+    "endRequestGuestAccess", "Ok. Goodbye.",
+    {
+        matches: /^cancel$|^goodbye$/i,
+        confirmPrompt: "This will cancel your request. Are you sure?"
+    }
+);
